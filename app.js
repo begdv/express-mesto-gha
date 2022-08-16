db41cdf184ea07789e76df94342ec8867c5fb600
Begunov Dmitry <begdv@yandex.ru>
2022-08-16 19:37:16 +0300

    Создать контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/users', require('./routes/users'));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
 });
