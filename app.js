89b1f48227fc631e65c8921369bbf0e62864c326
Begunov Dmitry <begdv@yandex.ru>
2022-08-16 14:48:23 +0300

    Подключиться к серверу MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-// Слушаем 3000 порт
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
