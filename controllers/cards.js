491c80e9de3dddf91a4588cd8821972e54412515
Begunov Dmitry <begdv@yandex.ru>
2022-08-24 22:11:36 +0300

    Сделать так, чтобы API не возвращал хеш пароля

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,7 +15,10 @@ module.exports.getCards = (req, res) => {
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => Card.findById(card._id)
+      .populate('owner')
+      .populate('likes')
+      .then((cardPopulated) => res.send({ data: cardPopulated })))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
         res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при создании карточки' });
