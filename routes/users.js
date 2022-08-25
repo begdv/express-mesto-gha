cfd07a5331635ad1c05ed65ae30bf524c591ed8e
Begunov Dmitry <begdv@yandex.ru>
2022-08-26 00:40:16 +0300

    Исправления по результатам тестов

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,14 @@ const {
 
 router.get('/', getUsers);
 
-router.get('/:userId', getUser);
-
 router.get('/me', getMe);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().min(24).max(24)
+      .required(),
+  }),
+}), getUser);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
